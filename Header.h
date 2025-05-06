d8b8973b2b8ce49d8babf7dc231f76e0c10942f5
TigGev <tigogevorgyan29@gmail.com>
2025-05-06 15:37:43 +0400

    Const_Iterator

---
+++ Header.h
@@ -0,0 +1,69 @@
+#ifndef ITERATOR_HEADER
+#define ITERATOR_HEADER
+#include <cstdio>
+#include <iterator>
+
+template <typename T>
+class Const_Iterator  {
+        const T* ptr;
+    public:
+    //  override the usings here if needed  
+        using value_type = T;
+        using difference_type = std::ptrdiff_t;
+        using reference = const T&;
+        using pointer = const T*;
+        using iterator_category = std::random_access_iterator_tag;
+
+        Const_Iterator(pointer p);
+        Const_Iterator(const Const_Iterator<T>& other);
+        Const_Iterator& operator= (const Const_Iterator& other);
+        reference operator*() const;
+        pointer operator->() const;
+        Const_Iterator& operator++();
+        Const_Iterator operator++(int); //postfix
+        Const_Iterator& operator--();
+        Const_Iterator operator--(int); //postfix
+        reference operator[](difference_type n) const;
+        Const_Iterator operator+(difference_type n) const;
+        Const_Iterator operator-(difference_type n) const;
+        friend Const_Iterator operator+(difference_type n, const Const_Iterator& it); 
+        difference_type operator-(const Const_Iterator& other) const;
+        Const_Iterator& operator+=(difference_type n);
+        Const_Iterator& operator-=(difference_type n);
+
+        bool operator==(const Const_Iterator& other) const;
+        bool operator!=(const Const_Iterator& other) const;
+        bool operator<=(const Const_Iterator& other) const;
+        bool operator>=(const Const_Iterator& other) const;
+        bool operator>(const Const_Iterator& other) const;
+        bool operator<(const Const_Iterator& other) const;
+
+};
+
+template <typename T>
+class Iterator : public Const_Iterator {
+        T* ptr;
+    public:
+//usings here at first 
+        using reference = T&;
+        using pointer =  T*;
+
+        Iterator(pointer);
+        Iterator(const Iterator& other);
+        reference operator*();
+        pointer operator->();
+        Iterator& operator++();
+        Iterator operator++(int); //postfix
+        Iterator& operator--();
+        Iterator operator--(int); //postfix
+        reference operator[](size_t n) const;
+        friend Iterator& operator+(difference_type n, Iterator& it);
+        Iterator& operator+(difference_type n) const;
+        Iterator& operator-(difference_type n) const;
+        Iterator& operator+=(difference_type n);
+        Iterator& operator-=(difference_type n);
+
+};
+
+
+#endif
---
+++ Implemantation.cpp
@@ -0,0 +1,141 @@
+#include <Header.h>
+#include <cstdio>
+
+//////////////////////////////////////// CONST ITERATOR ////////////////////////////////////////////////
+template <typename T>
+Const_Iterator<T>::Const_Iterator(pointer p) : ptr(p) {}
+
+template <typename T>
+Const_Iterator<T>::Const_Iterator(const Const_Iterator<T>& other) : Const_Iterator(other.ptr) {}
+
+template <typename T>
+Const_Iterator<T>& Const_Iterator<T>::operator= (const Const_Iterator<T>& other) {
+    ptr = other.ptr;
+    return *this;
+}
+
+template <typename T>
+typename Const_Iterator<T>::reference Const_Iterator<T>::operator*() const {return *ptr;}
+
+template <typename T>
+typename Const_Iterator<T>::pointer Const_Iterator<T>::operator->() const {return ptr;}
+
+template <typename T>
+Const_Iterator<T>& Const_Iterator<T>::operator++() {
+    ++ptr;
+    return *this;
+}
+
+template <typename T>
+Const_Iterator<T> Const_Iterator<T>::operator++(int) {
+    Const_Iterator tmp(*this);
+    ++ptr;
+    return tmp;
+}
+
+template <typename T>
+Const_Iterator<T>& Const_Iterator<T>::operator--() {
+    --ptr;
+    return *this;
+}
+
+template <typename T>
+Const_Iterator<T> Const_Iterator<T>::operator--(int) {
+    Const_Iterator tmp(*this);
+    --ptr;
+    return tmp;    
+}
+
+template <typename T>
+typename Const_Iterator<T>::reference Const_Iterator<T>::operator[](typename Const_Iterator<T>::difference_type n) const {
+    return *(ptr + n);
+}
+
+template <typename T>
+Const_Iterator<T> Const_Iterator<T>::operator+(difference_type n) const { return Const_Iterator(ptr + n);}
+
+template <typename T>
+Const_Iterator<T> operator+(typename Const_Iterator<T>::difference_type n, const Const_Iterator<T>& it) {
+    return Const_Iterator<T>(it.ptr + n);
+}
+
+template <typename T>
+Const_Iterator<T> Const_Iterator<T>::operator-(difference_type n) const {
+    return Const_Iterator<T>(ptr - n);
+}
+
+template <typename T>
+typename  Const_Iterator<T>::difference_type Const_Iterator<T>::operator-(const Const_Iterator<T>& other) const {
+    return ptr - other.ptr;
+}
+
+template <typename T>
+Const_Iterator<T>& Const_Iterator<T>::operator+=(difference_type n) {
+    ptr += n;
+    return *this;
+}
+
+template <typename T>
+Const_Iterator<T>& Const_Iterator<T>::operator-=(difference_type n) {
+    ptr -= n;
+    return *this;
+}
+
+
+
+template <typename T>
+bool Const_Iterator<T>::operator==(const Const_Iterator<T>& other) const {
+    return ptr == other.ptr;
+}
+
+template <typename T>
+bool Const_Iterator<T>::operator!=(const Const_Iterator<T>& other) const {
+    return ptr != other.ptr;
+}
+
+template <typename T>
+bool Const_Iterator<T>::operator<=(const Const_Iterator<T>& other) const {
+    return ptr <= other.ptr;
+}
+
+template <typename T>
+bool Const_Iterator<T>::operator>=(const Const_Iterator<T>& other) const {
+    return ptr >= other.ptr;
+}
+
+template <typename T>
+bool Const_Iterator<T>::operator<(const Const_Iterator<T>& other) const {
+    return ptr < other.ptr;
+}
+
+template <typename T>
+bool Const_Iterator<T>::operator>(const Const_Iterator<T>& other) const {
+    return ptr > other.ptr;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////  ITERATOR /////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename T>
+Iterator<T>::Iterator(pointer p) : Const_Iterator(p) {}
+
+template <typename T>
+Iterator<T>::Iterator(const Iterator<T>& other) : Const_Iterator(other.ptr) {}
+
+template <typename T>
+Iterator<T>::reference Iterator<T>::operator*() {return *ptr;}
+
+template <typename T>
+Iterator<T>::pointer Iterator<T>::operator->() {return ptr;}
+
+template <typename T>
+
+template <typename T>
+
+template <typename T>
+
+template <typename T>
+
+template <typename T>
+
